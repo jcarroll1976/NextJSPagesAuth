d05228d68667bfd3a54baebf05122aa2ff167a59
Joshua Carroll <carrolljosh1976@gmail.com>
2025-01-27 22:52:15 -0500

    fixed connectToDatabase function in db.js to create users in MongoDB

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,31 +1,49 @@
-import { hashPassword } from "../../../lib/auth";
-import { connectDatabase } from "../../../lib/db";
+import { hashPassword } from '../../../lib/auth';
+import { connectToDatabase } from '../../../lib/db';
 
-export default async function handler(req,res) {
-    const data = req.body;
+async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return;
+  }
 
-    const { email, password } = data;
+  const data = req.body;
 
-    if (
-        !email ||
-        !email.includes('@') ||
-        !password ||
-        password.trim().length < 7
-    ) {
-        res.status(422).json({ message: 'Invalid input - password should also be at least 7 characters long' });
-        return;
-    }
+  const { email, password } = data;
 
-    const client = await connectDatabase();
-
-    const db = client.db();
-
-    const hashPassword = await hashPassword(password);
-
-    const result = db.collection('users').insertOne({
-       email: email,
-       password: hashPassword
+  if (
+    !email ||
+    !email.includes('@') ||
+    !password ||
+    password.trim().length < 7
+  ) {
+    res.status(422).json({
+      message:
+        'Invalid input - password should also be at least 7 characters long.',
     });
+    return;
+  }
 
-    res.status(201).json({ message: 'Created user!' });
+  const client = await connectToDatabase();
+
+  const db = client.db();
+
+  const existingUser = await db.collection('users').findOne({ email: email });
+
+  if (existingUser) {
+    res.status(422).json({ message: 'User exists already!' });
+    client.close();
+    return;
+  }
+
+  const hashedPassword = await hashPassword(password);
+
+  const result = await db.collection('users').insertOne({
+    email: email,
+    password: hashedPassword,
+  });
+
+  res.status(201).json({ message: 'Created user!' });
+  client.close();
 }
+
+export default handler;
