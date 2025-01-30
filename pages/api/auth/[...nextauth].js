f740d8a17500fd3b799181884abf8bfd12cf2b06
Joshua Carroll <carrolljosh1976@gmail.com>
2025-01-30 10:42:51 -0500

    finished adding credentials auth provider and user login logic for backend

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,13 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
 import { connectToDatabase } from '../../../lib/db';
+import { verifyPassword } from '../../../lib/auth';
+import { session } from 'next-auth/client';
 
 export default NextAuth({
+    session: {
+        jwt: true
+    },
     providers: [
         Providers.Credentials({
             async authorize(credentials) {
@@ -16,7 +21,17 @@ export default NextAuth({
                     throw new Error('No user found!');
                 }
 
+                const inValid = verifyPassword(credentials.password, user.password);
+
+                if (!inValid) {
+                    client.close();
+                    throw new Error('Could not log you in!');
+                }
+
                 client.close();
+                return { email: user.email };
+
+                
             }
         })
     ]
